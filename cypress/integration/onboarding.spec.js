b1e169c4b79b53f4d05b49b0a68ca28380a4723a
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-14 14:43:39 -0500

    added test to check if elements exist

--- cypress/integration/onboarding.spec.js
+++ cypress/integration/onboarding.spec.js
@@ -3,5 +3,17 @@ describe('Onboarding App', () => {
     cy.visit('http://localhost:3000')
   })
 
-  it('sanity test', () => expect(true).to.equal(true))
+  const nameInput = () => cy.get('input[name=name]')
+  const emailInput = () => cy.get('input[name=email]')
+  const passwordInput = () => cy.get('input[name=password]')
+  const tosInput = () => cy.get('input[name=tos]')
+  const submitButton = () => cy.get('button')
+
+  it('should show the proper elements', () => {
+    nameInput().should('exist')
+    emailInput().should('exist')
+    passwordInput().should('exist')
+    tosInput().should('exist')
+    submitButton().should('exist')
+  })
 })
